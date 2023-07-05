8eda8a327b464170c4e862eeec85d5454c727a09
Camron Naderi <camronnaderi57@gmail.com>
2023-07-05 13:28:58 -0700

    sum the suubmissions for Ibrahim Schimmel

--- 1_queries/student_names_from1_cohort.sql
+++ 1_queries/student_names_from1_cohort.sql
@@ -1,5 +1,5 @@
-SELECT name, email, phone
+SELECT students.name, cohorts.name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
 FROM students
-WHERE github IS NULL 
-AND end_date IS NOT NULL;
-
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;

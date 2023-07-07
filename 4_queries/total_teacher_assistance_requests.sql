8864cdab02fe38baabf245e276fdf83c4ac6b12c
Camron Naderi <camronnaderi57@gmail.com>
2023-07-06 20:00:10 -0700

    BootcampX Queries 4 Challenge

---
+++ 4_queries/total_teacher_assistance_requests.sql
@@ -0,0 +1,5 @@
+SELECT teachers.name as name, COUNT(assistance_requests.*) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+WHERE name = 'Waylon Boehm'
+GROUP BY name;

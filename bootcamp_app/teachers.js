2f02a91dfe98fa38a6fea399c5747f54586e460f
Camron Naderi <camronnaderi57@gmail.com>
2023-07-14 17:04:04 -0700

    Implemented safe SQL queries using parameterized statements

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,12 +1,23 @@
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'camronnaderi',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;`
+
+const cohortName = [`%${process.argv[2]}%`];
+
+pool.query(queryString, cohortName)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
